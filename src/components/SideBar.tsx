ad4f5a67dcfbde377ec904239266fc168ab8542c
Lucas Duarte <lucasduarte@pecege.com>
2021-04-08 17:52:33 -0300

    refactor: componentization Sidebar and Content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+
+interface Genres {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  genres: Genres[];
+  selectedGenreId: number;
+  handleClickButton: (genreId: number) => void;
+}
+
+import { Button } from './Button';
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
